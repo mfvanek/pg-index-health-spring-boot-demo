b32cfc7db4a933cba934a05995a3fe9c62108324
Ivan Vakhrushev <IVakhrushev@innotechnum.com>
2021-05-30 11:46:17 +0300

    Initial commit

---
+++ src/main/java/io/github/mfvanek/pgindexhealthspringbootdemo/PgIndexHealthSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.mfvanek.pgindexhealthspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgIndexHealthSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgIndexHealthSpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mfvanek/pgindexhealthspringbootdemo/PgIndexHealthSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mfvanek.pgindexhealthspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgIndexHealthSpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
