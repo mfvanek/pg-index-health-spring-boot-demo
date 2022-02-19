031352e6b670dfba3522fe2e96066618f29b4dec
Ivan Vakhrushev <IVakhrushev@innotechnum.com>
2022-02-19 21:29:26 +0300

    Fixed checkstyle errors

--- src/main/java/io/github/mfvanek/pg/index/health/demo/controller/DefaultController.java
+++ src/main/java/io/github/mfvanek/pg/index/health/demo/controller/DefaultController.java
@@ -12,9 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Slf4j
 @RestController
