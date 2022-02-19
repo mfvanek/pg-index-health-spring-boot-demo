3e2c38e2911ec3cbb803b811fecae91aacc0452d
Ivan Vakhrushev <IVakhrushev@innotechnum.com>
2022-02-19 16:47:37 +0300

    Moved actuator to a separate port

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ Demo app for pg-index-health library (with Spring and Spring Boot).
 
 ## Endpoints
 ### Health
-http://localhost:8080/api/actuator/health
+http://localhost:8081/actuator/health
+
+### Info
+http://localhost:8081/actuator/info
 
 ### Metrics
-http://localhost:8080/api/actuator/prometheus
+http://localhost:8081/actuator/prometheus
 
 ### Liquibase
-http://localhost:8080/api/actuator/liquibase
+http://localhost:8081/actuator/liquibase
--- src/test/java/io/github/mfvanek/pg/index/health/demo/ActuatorEndpointTest.java
+++ src/test/java/io/github/mfvanek/pg/index/health/demo/ActuatorEndpointTest.java
@@ -3,52 +3,70 @@ package io.github.mfvanek.pg.index.health.demo;
 import io.github.mfvanek.pg.index.health.demo.utils.BasePgIndexHealthDemoSpringBootTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.web.server.LocalManagementPort;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ActuatorEndpointTest extends BasePgIndexHealthDemoSpringBootTest {
 
     @LocalServerPort
     private int port;
 
+    @LocalManagementPort
+    private int actuatorPort;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
-    private static final String ACTUATOR_URL_TEMPLATE = "http://localhost:%s/api/actuator/%s";
+    private static final String ACTUATOR_URL_TEMPLATE = "http://localhost:%s/actuator/%s";
+
+    @Test
+    void actuatorShouldBeRunOnSeparatePort() {
+        assertThat(actuatorPort).isNotEqualTo(port);
+    }
 
     @Test
     void prometheusEndpointShouldReturnMetrics() {
-        final String url = String.format(ACTUATOR_URL_TEMPLATE, port, "prometheus");
+        final String url = String.format(ACTUATOR_URL_TEMPLATE, actuatorPort, "prometheus");
         final ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
         assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
-        assertNotNull(response.getBody());
+        assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody()).contains("jvm_threads_live_threads");
     }
 
     @Test
     void healthEndpointShouldReturnStatusUp() {
-        final String url = String.format(ACTUATOR_URL_TEMPLATE, port, "health");
+        final String url = String.format(ACTUATOR_URL_TEMPLATE, actuatorPort, "health");
         final ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
         assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
-        assertNotNull(response.getBody());
-        assertThat(response.getBody()).contains("{\"status\":\"UP\"}");
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody()).contains("{\"status\":\"UP\",\"groups\":[\"liveness\",\"readiness\"]}");
     }
 
     @Test
     void liquibaseEndpointShouldReturnOk() {
-        final String url = String.format(ACTUATOR_URL_TEMPLATE, port, "liquibase");
+        final String url = String.format(ACTUATOR_URL_TEMPLATE, actuatorPort, "liquibase");
         final ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
         assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
-        assertNotNull(response.getBody());
+        assertThat(response.getBody()).isNotNull();
         assertThat(response.getBody()).contains(
                 "{\"contexts\":{\"pg-index-health-spring-boot-demo\":{\"liquibaseBeans\":{\"liquibase\":{\"changeSets\"");
+    }
+
+    @Test
+    void infoEndpointShouldReturnStatusUp() {
+        final String url = String.format(ACTUATOR_URL_TEMPLATE, actuatorPort, "info");
+        final ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+        assertThat(response.getStatusCode()).isEqualByComparingTo(HttpStatus.OK);
+        assertThat(response.getBody()).isNotNull();
+        assertThat(response.getBody()).contains("\"version\":");
     }
 }
