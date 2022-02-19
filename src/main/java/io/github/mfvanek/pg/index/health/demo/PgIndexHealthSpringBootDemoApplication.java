0e043ad2a9ec16bf7a30e27f9ade5fb945f2e661
Ivan Vakhrushev <IVakhrushev@innotechnum.com>
2022-02-19 17:12:50 +0300

    Added checkstyle

--- src/main/java/io/github/mfvanek/pg/index/health/demo/PgIndexHealthSpringBootDemoApplication.java
+++ src/main/java/io/github/mfvanek/pg/index/health/demo/PgIndexHealthSpringBootDemoApplication.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019-2022. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health-spring-boot-demo
+ *
+ * Licensed under the Apache License 2.0
+ */
+
 package io.github.mfvanek.pg.index.health.demo;
 
 import org.springframework.boot.SpringApplication;
