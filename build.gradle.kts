42af2f4bbebee2e1814199018f6b268319d5d28c
i.n.vakhrushev <i.n.vakhrushev@tinkoff.ru>
2023-08-04 15:29:20 +0400

    Updated Gradle to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,9 @@ import net.ltgt.gradle.errorprone.errorprone
 
 plugins {
     id("java")
-    id("org.springframework.boot") version "2.7.13"
+    id("org.springframework.boot") version "2.7.14"
     id("io.spring.dependency-management") version "1.1.2"
-    id("com.github.spotbugs") version "5.0.14"
+    id("com.github.spotbugs") version "5.1.1"
     id("checkstyle")
     id("jacoco")
     id("pmd")
